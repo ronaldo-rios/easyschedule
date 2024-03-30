a5b676d35e4de3c824a89594f5bd0201b100e740
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-03-30 11:57:41 -0300

    Criação de tables de niveis de acesso das paginas e correção de bug de encoded na criação via docker

--- sql/mysql/init.sql
+++ sql/mysql/init.sql
@@ -2,6 +2,7 @@ CREATE DATABASE easy_schedule
     CHARACTER SET utf8
     COLLATE utf8_general_ci;
 
+SET NAMES utf8;
 USE easy_schedule;
 
 CREATE TABLE `access_levels` (
@@ -66,10 +67,86 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_users_with_user_situation_id`
   FOREIGN KEY (`user_situation_id`) REFERENCES `users_situation`(`id`) 
-  ON DELETE RESTRICT ON UPDATE CASCADE
+    ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_users_with_access_level_id`
   FOREIGN KEY (`access_level_id`) REFERENCES `access_levels`(`id`)
-  ON DELETE RESTRICT ON UPDATE CASCADE
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE `page_status`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `status` VARCHAR(100) NOT NULL,
+    `color_id` INT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_page_status_with_color_id`
+    FOREIGN KEY (`color_id`) REFERENCES `colors`(`id`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE `page_groups` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `group_name` VARCHAR(225) NOT NULL,
+    `order_page_group` INT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `page_modules` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(225) NOT NULL,
+    `name` VARCHAR(225) NOT NULL,
+    `order_module` INT NOT NULL,
+    `obs` TEXT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `pages` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `controller` VARCHAR(225) NOT NULL,
+    `method` VARCHAR(225) NOT NULL,
+    `controller_in_the_main` VARCHAR(225) NOT NULL,
+    `method_in_the_main` VARCHAR(225) NOT NULL,
+    `name_page` VARCHAR(225) NOT NULL,
+    `public` INT NOT NULL,
+    `icon` VARCHAR(225) NULL,
+    `obs` TEXT NULL,
+    `page_status_id` INT NOT NULL,
+    `page_group_id` INT NOT NULL,
+    `page_module_id` INT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_pages_with_page_status_id`
+    FOREIGN KEY (`page_status_id`) REFERENCES `page_status`(`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_pages_with_page_group_id`
+    FOREIGN KEY (`page_group_id`) REFERENCES `page_groups`(`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_pages_with_page_module_id`
+    FOREIGN KEY (`page_module_id`) REFERENCES `page_modules`(`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE `page_levels`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `permission` INT NOT NULL,
+    `order_level_page` INT NOT NULL,
+    `access_level_id` INT NOT NULL,
+    `page_id` INT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_page_levels_with_access_level_id`
+    FOREIGN KEY (`access_level_id`) REFERENCES `access_levels`(`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT `fk_page_levels_with_page_id`
+    FOREIGN KEY (`page_id`) REFERENCES `pages`(`id`)
+        ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 INSERT INTO `colors`(color_name, color, created_at) VALUES('Azul', '#0275D8', NOW());
@@ -90,9 +167,25 @@ INSERT INTO `access_levels`(`access_level`, `order_level`, `created_at`) VALUES(
 INSERT INTO `access_levels`(`access_level`, `order_level`, `created_at`) VALUES('Usuário Default', 3, NOW());
 INSERT INTO `access_levels`(`access_level`, `order_level`, `created_at`) VALUES('Financeiro', 4, NOW());
 
-INSERT INTO `users` (`name`, `nickname`, `email`, `user`, `password`, `user_situation_id`, `created_at`)
-VALUES 
-('User to Test', 'USERTOTEST', 'suporte@teste.com', 'USERTEST' , '$2y$10$hqgJV15KX4k8e06PY.aL7OqSnHA0at.ng5iamwGKBpcJRDdYLCSB2', 2, NOW());
+INSERT INTO `page_status`(status, color_id, created_at) VALUES('Ativo', 3, NOW());
+INSERT INTO `page_status`(status, color_id, created_at) VALUES('Inativo', 4, NOW());
+
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Listar', 1, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Visualizar', 2, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Cadastrar', 3, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Editar', 4, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Excluir', 5, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Acesso', 6, NOW());
+INSERT INTO `page_groups`(group_name, order_page_group, created_at) VALUES('Outros', 7, NOW());
+
+INSERT INTO `page_modules`(type, name, order_module, obs, created_at) VALUES('adms', 'Administrativo', 1, 'Administração e configurações de Usuários', NOW());
+INSERT INTO `page_modules`(type, name, order_module, obs, created_at) VALUES('sche', 'Agendamentos', 2, 'Administração de agendamentos', NOW());
+INSERT INTO `page_modules`(type, name, order_module, obs, created_at) VALUES('finc', 'Financeiro', 3, 'Administração de finanças', NOW());
+
+INSERT INTO `users` 
+(`name`, `nickname`, `email`, `user`, `password`, `user_situation_id`, `access_level_id`, `created_at`)
+VALUES -- Senha do usuário padrão sem o hash: Secret123
+('User to Test', 'USERTOTEST', 'suporte@teste.com', 'USERTEST' , '$2y$10$XF23pikBWucg6xf.8RJJFebMm/2uWKLUXS6V2vaJXrhsJDA0a0nS2', 1, 1, NOW());
 
 -- Example of email configuration. Select the email configuration that you want to use and insert the data in the table config_emails
 -- INSERT INTO `config_emails` (`title`, `name`, `email`, `host`, `username`, `password`, `smtp_secure`, `port`, `created_at`)
